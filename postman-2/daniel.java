9b4d523e5cd864b2ee9e85190f2199266f84e6d4
Drugecuellar <94062723+Drugecuellar@users.noreply.github.com>
2022-02-18 11:37:49 -0500

    Add files via upload

---
+++ postman-2/daniel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto2;
+
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mac
+ */
+public class daniel {
+    public static void main(String[] args) {
+        
+    }
+}
